a3196339be86c13089b3725b3b4d62d147e0947f
yerez29 <yerez.29@gmail.com>
2019-04-19 12:45:12 +0300

    finished ex3

--- app/src/main/java/com/example/firststeptoselfchat/MainActivity.java
+++ app/src/main/java/com/example/firststeptoselfchat/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.firststeptoselfchat;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener{
     public static final String EMPTY_STRING = "";
     public static final String ERROR_MSG = "you can't send an empty message, oh silly!";
+    private static final String LENGTH_OF_LIST = "length_of_list";
+    private static final String LIST_OF_MESSAGES = "list_of_messages";
     public MyRecyclerViewAdapter adapter;
     private ArrayList<String> messages = new ArrayList<>();
+    private int lstLength;
+    final Gson gson = new Gson();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +41,16 @@ public class MainActivity extends AppCompatActivity{
         CharSequence text = ERROR_MSG;
         int duration = Toast.LENGTH_SHORT;
         final Toast toast = Toast.makeText(context, text, duration);
-//        final ArrayList<String> messages = new ArrayList<>();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        lstLength = sp.getInt(LENGTH_OF_LIST, 0);
+        final SharedPreferences.Editor editor = sp.edit();
+        if (lstLength != 0)
+        {
+            String rjson = sp.getString(LIST_OF_MESSAGES, EMPTY_STRING);
+            Type type = new TypeToken<List<String>>() {}.getType();
+            messages = gson.fromJson(rjson, type);
+        }
+        Log.v(LENGTH_OF_LIST, String.valueOf(lstLength));
         final RecyclerView recyclerView = findViewById(R.id.my_recycle_view);
         if (messages.size() > 0)
         {
@@ -38,6 +59,7 @@ public class MainActivity extends AppCompatActivity{
         final LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
         adapter = new MyRecyclerViewAdapter(this, messages);
+        adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
                 manager.getOrientation());
@@ -55,13 +77,50 @@ public class MainActivity extends AppCompatActivity{
                     messages.add(outputStr);
                     adapter.notifyDataSetChanged();
                     recyclerView.scrollToPosition(messages.size() - 1);
+                    lstLength += 1;
+                    editor.putInt(LENGTH_OF_LIST, lstLength);
+                    String wjson = gson.toJson(messages);
+                    editor.putString(LIST_OF_MESSAGES, wjson);
+                    editor.apply();
                 }
             }
         });
     }
     @Override
+    public void onItemClick(final View view, final int position, boolean isLongClick) {
+        if (!isLongClick)
+        {
+            return;
+        }
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        messages.remove(position);
+                        adapter.notifyItemRemoved(position);
+                        lstLength -= 1;
+                        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                        final SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt(LENGTH_OF_LIST, lstLength);
+                        String wjson = gson.toJson(messages);
+                        editor.putString(LIST_OF_MESSAGES, wjson);
+                        editor.apply();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+    }
+    @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putStringArrayList("key", messages);
+
         super.onSaveInstanceState(savedInstanceState);
     }
 
@@ -69,9 +128,5 @@ public class MainActivity extends AppCompatActivity{
     public void onRestoreInstanceState(Bundle savedInstanceState) {
 
         super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState.getStringArrayList("key") != null)
-        {
-            messages.addAll(savedInstanceState.getStringArrayList("key"));
-        }
     }
 }
--- app/src/main/java/com/example/firststeptoselfchat/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/firststeptoselfchat/MyRecyclerViewAdapter.java
@@ -5,13 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import java.util.List;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
 
     private List<String> recycleData;
     private LayoutInflater recycleInflater;
+    private ItemClickListener mClickListener;
 
     MyRecyclerViewAdapter(Context context, List<String> data) {
         this.recycleInflater = LayoutInflater.from(context);
@@ -35,11 +35,35 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         return recycleData.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
         TextView myTextView;
         ViewHolder(View itemView) {
             super(itemView);
             myTextView = itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition(), false);
+        }
+
+        public boolean onLongClick(View view){
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition(), true);
+            return true;
+        }
+    }
+
+    String getItem(int id) {
+        return recycleData.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position, boolean isLongClick);
     }
 }
